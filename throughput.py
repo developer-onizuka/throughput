d8aa6c7cd0d904d64c8a9601cad7b81ce9595288
developer-onizuka <his.onioni@gmail.com>
2021-01-03 09:05:03 +0900

    2021-01-03

--- throughput.py
+++ throughput.py
@@ -9,33 +9,50 @@ from time import sleep
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 args = sys.argv
 mode = args[1] 
-# server side: ./pingpong.py server
-# cilent side: ./pingpong.py <server's ip address>
+# server side: ./throughput.py server
+# cilent side: ./throughput.py <server's ip address>
 
 size = 1
 maxsize = 1024*1024
 limit = math.log(maxsize,2)
 
 if mode == "server": 
+	#s.bind(('127.0.0.1', 5000))
 	s.bind((socket.gethostname(), 5000))
 	s.listen(5)
+	s.setblocking(1)
 	print('listening')
 	conn, addr = s.accept()
-	while True:
-		rbuf = conn.recv(maxsize)
+	for _ in range(int(limit)+1):
+		iter = 100 if size < 1024*8 else(10 if size <1024*64 else 1)
+		#while True:
+		for _ in range(iter):
+			#rbuf = conn.recv(maxsize)
+			rbuf = conn.recv(size)
+			#print rbuf
+			#if not rbuf: break	
+			#conn.sendall(b'ack')
+			conn.sendall(rbuf)
+			#print size
+		print(size,'[B],\t')
+		size *= 2
 	conn.close()
 	s.close()
 else:
 	s.connect((mode, 5000))
 	for _ in range(int(limit)+1):
-		data = np.array([x for x in range(size)], dtype=str)
-		iter = 100000 if size < 512 else(1000 if size <1024*32 else 10)
+		data = np.array([1 for x in range(size)], dtype=str)
+		iter = 100 if size < 1024*8 else(10 if size <1024*64 else 1)
 		start = time.time()
 		for _ in range(iter):
-			s.send(data)
-		elasped_time = time.time() - start
+			s.sendall(data)
+			#tmp = 'nak'
+			#print tmp
+			rbuf = s.recv(size)
+			#print tmp
+		elasped_time = (time.time() - start)/2
 		th = 8 * data.size * iter / elasped_time / 1024 / 1024
-		print data.size,'[B],\t', elasped_time,'[s],\t', iter,'[loops],\t', th,'[Mbps],'
+		print(data.size,'[B],\t', elasped_time,'[s],\t', iter,'[loops],\t', th,'[Mbps],')
 		size *= 2
 	s.close()
 
